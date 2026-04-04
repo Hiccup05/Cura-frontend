b3d0e87cb320ba06f9eb63a79cb5abc133c73a0a
Hiccup05 <paudelbipin05Z@gmail.com>
2026-04-04 12:45:52 +0545

    add admin doctor detail

---
+++ src/pages/admin/AdminDoctorDetail.tsx
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import {
+    Card, Table, Button, Modal, Form, Input,
+    Select, Tag, Typography, message, Popconfirm, TimePicker, DatePicker
+} from 'antd';
+import { PlusOutlined, DeleteOutlined, EditOutlined, ArrowLeftOutlined } from '@ant-design/icons';
+import dayjs from 'dayjs';
+import api from '../../services/api';
+import { Doctor, Schedule, Leave, DayOfWeek, Specialization } from '../../types/admin';
+
+const { Title } = Typography;
