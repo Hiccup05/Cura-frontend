b8b4da0a7036b6b754ebc9e257834740981e33c6
Hiccup05 <paudelbipin05Z@gmail.com>
2026-04-04 13:28:52 +0545

    create BloodGroup type

--- src/types/patient.ts
+++ src/types/patient.ts
@@ -1,1 +1,5 @@
 export type Gender = 'MALE' | 'FEMALE';
+
+export type BloodGroup = 'A_POSITIVE' | 'A_NEGATIVE' | 'B_POSITIVE' | 'B_NEGATIVE' |
+    'O_POSITIVE' | 'O_NEGATIVE' | 'AB_POSITIVE' | 'AB_NEGATIVE'
+
